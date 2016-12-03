44d93f67c1c484e43b8612a74f159ac8b6a28d8f
davidangel <david@davidangel.net>
2016-12-03 14:09:14 -0500

    Update MeetingList component to default to listing 5 meetings.

--- resources/assets/js/components/MeetingList.vue
+++ resources/assets/js/components/MeetingList.vue
@@ -4,6 +4,10 @@
             <div v-for="meeting in meetings">
                 <meeting :meeting="meeting"></meeting>
             </div>
+        <div class="text-center">
+            <a v-if="!showingAll" v-on:click="fetchData(0)" class="btn btn-default" href="#">Show All Meetings</a>
+            <a v-if="showingAll" v-on:click="fetchData(5)" class="btn btn-default" href="#">Show Last 5 Meetings</a>
+        </div>
     </div>
 </template>
 <script>
@@ -12,15 +16,18 @@
         components: { Meeting },
         data(){
             return{
-                meetings: []
+                meetings: [],
+                showingAll: false
             }
         },
         created() {
-            this.fetchData();
+            this.fetchData(5);
         },
         methods: {
-            fetchData: function () {
-                this.$http.get('/api/meetings').then((response) => {
+            fetchData: function (limit) {
+                if (limit == null){ var limit = 0; }
+                if (limit == 0){ this.showingAll = true; } else { this.showingAll = false; }
+                this.$http.get('/api/meetings?limit=' + limit).then((response) => {
                     this.meetings = response.body;
                 });
             }
