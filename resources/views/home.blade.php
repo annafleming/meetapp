ea78982f79aedd1407f9c43defdf83375fa15980
davidangel <david@davidangel.net>
2016-12-02 22:14:28 -0500

    Update home view to use more container width.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-7 col-md-push-4 col-sm-8 col-sm-push-4">
+        <div class="col-md-9 col-md-push-3 col-sm-8 col-sm-push-4">
             <meeting-list></meeting-list>
         </div>
 
-        <div class="col-md-3 col-md-pull-7 col-md-offset-1 col-sm-4 col-sm-pull-8 col-sm-offset-0">
+        <div class="col-md-3 col-md-pull-9 col-sm-4 col-sm-pull-8 col-sm-offset-0">
             <user-list></user-list>
         </div>
 
