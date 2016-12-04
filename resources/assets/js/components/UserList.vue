d6b3a9cc2fa02b63c475ebdf72fd008e94c88f38
davidangel <david@davidangel.net>
2016-12-04 18:16:26 -0500

    Add selectedUsers array and toggle for UserList.

--- resources/assets/js/components/UserList.vue
+++ resources/assets/js/components/UserList.vue
@@ -2,7 +2,7 @@
     <div>
             <h3>Users</h3>
             <div class="list-group">
-                <a v-for="user in users" type="button" class="list-group-item" href="#">{{ user.name }}</a>
+                <a v-for="user in users" @click="toggleSelect(user)" type="button" class="list-group-item" :class="{ active: selectedUsers.indexOf(user) !== -1 }" href="#">{{ user.name }}</a>
             </div>
     </div>
 </template>
@@ -13,7 +13,8 @@
     export default {
         data() {
             return {
-                users: []
+                users: [],
+                selectedUsers: []
             }
         },
         created(){
@@ -24,6 +25,14 @@
                 this.$http.get('/api/user').then((response) => {
                     this.users = response.body;
                 });
+            },
+            toggleSelect: function(user) {
+                var index = this.selectedUsers.indexOf(user);
+                if (index === -1) {
+                    this.selectedUsers.push(user);
+                } else {
+                    this.selectedUsers.splice(index, 1);
+                }
             }
         }
     }
