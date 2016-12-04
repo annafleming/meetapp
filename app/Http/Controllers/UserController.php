f482ab1e5b5f826b6af2a5d8803fda61b14ee57e
Anna Fleming <afleming@petsafe.net>
2016-12-04 14:28:10 -0500

    Exclude logged in user from the user list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -14,7 +15,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        return User::all();
+        return User::where('id', '!=', Auth::id())->get();
     }
 
     /**
